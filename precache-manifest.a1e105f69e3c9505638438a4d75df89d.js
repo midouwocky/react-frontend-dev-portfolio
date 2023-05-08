5845ca20b09abda6bffd6431ffa4b77fc3ca5a4a
Ahmed <ahmadou2808@gmail.com>
2023-05-08 12:55:16 +0100

    Updates

---
+++ precache-manifest.a1e105f69e3c9505638438a4d75df89d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2699283161881a2ed63640810835d108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "310b2b89b408c6de4452",
+    "url": "/static/css/2.941c1bbe.chunk.css"
+  },
+  {
+    "revision": "9e7056f328c833127d78",
+    "url": "/static/css/main.448cd1b1.chunk.css"
+  },
+  {
+    "revision": "310b2b89b408c6de4452",
+    "url": "/static/js/2.516c52d3.chunk.js"
+  },
+  {
+    "revision": "e8244eb9a46badc7470c28233f7bd690",
+    "url": "/static/js/2.516c52d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7056f328c833127d78",
+    "url": "/static/js/main.29d38e8e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
